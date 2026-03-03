698f337f5d79fc760e692a3987ca339eb5c4c4a5
soumyakantamitra <soumyakantamitra89@gmail.com>
2026-03-03 22:17:02 +0530

    fix: improve accessibility of trackers

--- trackers.py
+++ trackers.py
@@ -1,61 +1,48 @@
 from extractTorrentData import ExtractTorrentData
 from bencoding import encode, decode
 import requests
-import urllib.parse
 import hashlib
 import socket
 import struct
 import os
 
-filePath = r"C:\Users\Lenovo\Downloads\Fedora-Budgie-Live-x86_64-43.torrent"
+def getInfoHash(data):
+    encodedInfo = encode(data)
+    return hashlib.sha1(encodedInfo).digest()
 
-def get_info_hash(data):
-    encoded_info = encode(data)
-    return hashlib.sha1(encoded_info).digest()
-
-def get_size(info):
+def getSize(info):
     if b'length' in info:  #in case of single file
         return info[b'length']
     else:                  #in case of multiple files
         return sum(file[b'length'] for file in info[b'files'])
 
-def parse_peers(peer_bytes):
+def parsePeers(peerBytes):
     peers = []
-    for i in range(0, len(peer_bytes), 6):
-        ip = socket.inet_ntoa(peer_bytes[i:i+4])
-        port = struct.unpack(">H", peer_bytes[i+4:i+6])[0]
+    for i in range(0, len(peerBytes), 6):
+        ip = socket.inet_ntoa(peerBytes[i:i+4])
+        port = struct.unpack(">H", peerBytes[i+4:i+6])[0]
         peers.append((ip, port))
     return peers
 
+def getHandshakeData(filePath):
+    announceUrl, info = ExtractTorrentData(filePath)
+    peerId = b'-CC0101-' + os.urandom(12)
 
-announce_url, info = ExtractTorrentData(filePath)
+    infoHash = getInfoHash(info)
+    left = getSize(info)
+    params = {
+        'info_hash': infoHash,
+        'peer_id': peerId,
+        'port': 6881,
+        'uploaded': 0,
+        'downloaded': 0,
+        'left': left,
+        'compact': 1
+    }
 
-peer_id = b'-CC0101-' + os.urandom(12)
+    response = requests.get(announceUrl, params=params)
+    trackerResponse = decode(response.content)
+    peers = parsePeers(trackerResponse[b'peers'])
 
-info_hash = get_info_hash(info)
-left = get_size(info)
-params = {
-    'info_hash': info_hash,
-    'peer_id': peer_id,
-    'port': 6881,
-    'uploaded': 0,
-    'downloaded': 0,
-    'left': left,
-    'compact': 1
-}
+    return infoHash, peerId, peers
 
-response = requests.get(announce_url, params=params)
-
-tracker_response = decode(response.content)
-
-peers = parse_peers(tracker_response[b'peers'])
-
-if __name__ == "__main__":
-    print(left)
-    print(announce_url)
-    print("Status:", response.status_code, response.reason)
-    print(response.url)
-    print(tracker_response)
-    print("Got", len(peers), "Peers")
-    for i, (ip, port) in enumerate(peers):
-        print(f"Peer {i} is ip: {ip} port: {port}")
