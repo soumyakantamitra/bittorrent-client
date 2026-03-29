c935052b677285f93daa2a9c0d78e5e770b5666b
soumyakantamitra <soumyakantamitra89@gmail.com>
2026-03-29 23:28:32 +0530

    fix: remove unnecessary comments

--- bencoding.py
+++ bencoding.py
@@ -10,8 +10,3 @@ def encode(value):
 def decode(value):
   return bc.decode(value)
 
-
-# print(bc.encode("abd"))
-# print(bc.decode("i20e"))
-# print(bencodepy.decode('d5:title7:Examplee'))
-# print(bc.decode('d5:title7:Examplee'))
